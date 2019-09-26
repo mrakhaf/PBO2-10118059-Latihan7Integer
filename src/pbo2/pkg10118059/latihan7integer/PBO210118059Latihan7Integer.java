070c8ed67ac1b32e5110aed4e66b85d265dd8c4e
mrakhaf <48128093+mrakhaf@users.noreply.github.com>
2019-09-26 17:09:28 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118059/latihan7integer/PBO210118059Latihan7Integer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118059.latihan7integer;
+
+/**
+ *
+ * @author 
+ * Nama  : Muhammad Rakha Firdaus
+ * NIM   : 10118059
+ * Kelas : IF-2
+ */
+public class PBO210118059Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 9000000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
